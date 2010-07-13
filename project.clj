06f1b5ba727a0caa6e8e0976f6456883a077300c
Jeremy Wall <jeremy@marzhillstudios.com>
2010-07-13 18:45:19 -0500

    added lein/clojars and lein/swank to the dev dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
   :dev-dependencies [
-    [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+    [leiningen/lein-swank "1.2.0-SNAPSHOT"]
+    [lein-clojars "0.5.0-SNAPSHOT"]])
 
