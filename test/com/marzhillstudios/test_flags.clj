d9d826fc0b08c27b86112637b66c511e81df946c
Jeremy Wall <jeremy@marzhillstudios.com>
2010-07-13 20:44:43 -0500

    added unit-tests

---
+++ test/com/marzhillstudios/test_flags.clj
@@ -0,0 +1,47 @@
+(ns test-flags
+  (:use clojure.test
+        com.marzhillstudios.flags))
+
+(defn set-up []
+  (dosync (ref-set com.marzhillstudios.flags/*flags* {}))
+  (defflag Boolean "foo")
+  (defflag String "baz")
+  (defflag Integer "bar"))
+
+(deftest test-bool-flag?
+  (set-up)
+  (is (bool-flag? "--foo"))
+  )
+
+(deftest test-parse
+  (set-up)
+  (is (= (parse ["--nofoo"])
+         [{"foo" false} []]))
+  (is (= (parse ["--foo" ])
+         [{"foo" true} []]))
+  (is (= (parse ["--foo" "--bar=1" "--baz=razzle" "--" "cmd" "arg1"])
+         [{"foo" true "bar" 1 "baz" "razzle"} ["cmd" "arg1"]]))
+  (is (= (parse ["--nofoo" "--bar" "1" "--baz" "razzle" "--" "cmd" "arg1"])
+         [{"foo" false "bar" 1 "baz" "razzle"} ["cmd" "arg1"]]))
+  )
+
+(deftest test--test-flag-pair
+  (set-up)
+  (is (= (test-flag-pair "--bar" "1")
+         "--bar=1"))
+  (is (= (test-flag-pair "--bar" "1")
+         "--bar=1"))
+  (is (nil? (test-flag-pair "--bar=1" "--foo")))
+  (is (try (test-flag-pair "--bar" "--foo")
+           "--bar=1"
+           (catch IllegalStateException e
+             true)
+           (catch Exception e
+             false)))
+  (is (try (test-flag-pair "bar" "--foo")
+           "--bar=1"
+           (catch IllegalStateException e
+             true)
+           (catch Exception e
+             false)))
+  )
